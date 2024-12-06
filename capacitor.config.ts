0ea723809e38db9cdabe82171ed14153468e1340
@Sanoj.de.silva## <sanojdesilva24065@gmail.com>
2024-12-06 21:39:05 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'personal-trainer-app',
+  webDir: 'www'
+};
+
+export default config;
